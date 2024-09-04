871ef0f50089561c99245dc63d4f7390b799af1d
ellenfel <ellfelfen@gmail.com>
2024-09-05 02:01:30 +0300

    renamed some files code crashed + added some lessons

---
+++ freeCodeCamp.org/python_beginners_mini_projects/ch1_3.py
@@ -0,0 +1,33 @@
+
+# Lesson 1
+
+greeting = 'Hello World!'
+print(greeting)
+
+
+# Lesson 2
+
+line01 = "********************"  # header / footer
+line02 = "*                  *"  # re-use
+line03 = "*     WELCOME!     *"
+
+# starts with a blank line
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+
+
+# Lesson 3
+meaning = 42
+print('')
+
+# if meaning > 10:
+#     print('Right on!')
+# else:
+#     print('Not today')
+
+# Ternary Operator
+print('Right on!') if meaning > 10 else print('Not today')
