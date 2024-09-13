f1849a4bbd7e448306dfca1659441020f763835a
ellenfel <ellfelfen@gmail.com>
2024-09-13 11:07:56 +0300

    added closure chapter

--- freeCodeCamp.org/python_beginners_mini_projects/ch11_scope.py
+++ freeCodeCamp.org/python_beginners_mini_projects/ch11_scope.py
@@ -1,6 +1,19 @@
 #scope
+"""
+This module demonstrates the concept of scope in Python.
+
+Scopes in Python:
+- Global Scope: Variables defined at the top level of a script or module.
+- Local Scope: Variables defined inside a function.
+- Enclosing Scope: Variables in the local scope of enclosing functions.
+- Built-in Scope: Names preassigned in the built-in names module.
+
+Functions:
+- another(): Demonstrates the use of global and nonlocal keywords to modify variables in different scopes.
+"""
 
 
+# global scope
 name = "Dave"
 count = 1
 
