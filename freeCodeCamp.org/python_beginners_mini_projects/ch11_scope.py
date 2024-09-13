b63a238e4e1bc4c0ac025de7c4c1aad8a5ed34f8
ellenfel <ellfelfen@gmail.com>
2024-09-13 11:04:31 +0300

    scope.py added

--- freeCodeCamp.org/python_beginners_mini_projects/ch11_scope.py
+++ freeCodeCamp.org/python_beginners_mini_projects/ch11_scope.py
@@ -1,1 +1,23 @@
 #scope
+
+
+name = "Dave"
+count = 1
+
+
+def another():
+    color = "blue"
+    global count
+    count += 1
+    print(count)
+
+    def greeting(name):
+        nonlocal color
+        color = "red"
+        print(color)
+        print(name)
+
+    greeting("Dave")
+
+
+another()
