8b36f4e2fc8a62264b7abd4da3f9391c0da99184
ellenfel <ellfelfen@gmail.com>
2024-09-25 17:44:10 +0300

    added practice challanges 4 later

---
+++ freeCodeCamp.org/python_beginners_mini_projects/ch16_simple_practices/arcade.py
@@ -0,0 +1,17 @@
+"""
+Ask user to choose a game to play:
+    - Rock, Paper, Scissors
+    - Guess the Number
+    - Exit
+
+Get user's name as an argument from the command line.
+
+Load the selected game module and start the game.
+
+When the game is over, ask the user if they want to exit the game return to the arcade.
+
+"""
+
+import argparse
+import os
+import sys
---
+++ freeCodeCamp.org/python_beginners_mini_projects/ch16_simple_practices/guess_number.py
@@ -0,0 +1,29 @@
+"""
+ask the user to guess a number between 1 and 3
+
+generate a random number between 1 and 3
+
+check the user's guess
+
+if it matches the number generated, display "You guessed correctly"
+
+otherwise display "Wrong! Better luck next time"
+
+display the randomly generated number
+
+display winning percentage
+
+ask if the user wants to play again
+
+get user's name as an argument from the command line.
+"""
+
+import random
+import sys
+import argparse 
+import os
+
+
+
+
+# python guess_number.py -n Alice
