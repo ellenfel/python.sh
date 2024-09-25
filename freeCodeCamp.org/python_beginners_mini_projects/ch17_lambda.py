61f8577ed2be8f7f35ddc1545f29d3b3395d77f5
ellenfel <ellfelfen@gmail.com>
2024-09-25 17:48:31 +0300

    added labda function chaptter

---
+++ freeCodeCamp.org/python_beginners_mini_projects/ch17_lambda.py
@@ -0,0 +1,5 @@
+"""
+Lambda functions are small anonymous functions defined with the lambda keyword.
+They are often used for short, throwaway functions that are not reused elsewhere in the code.
+Lambda functions can take any number of arguments, but can only have one expression.
+"""
