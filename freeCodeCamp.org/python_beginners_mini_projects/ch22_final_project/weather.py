aa52f6f97897eae97828a4a0f25d60d49da93c04
ellenfel <ellfelfen@gmail.com>
2025-02-15 12:33:07 +0300

    fixed it

--- freeCodeCamp.org/python_beginners_mini_projects/ch22_final_project/weather.py
+++ freeCodeCamp.org/python_beginners_mini_projects/ch22_final_project/weather.py
@@ -5,17 +5,25 @@ import os
 
 load_dotenv()
 
+
 def get_current_weather(city="Kansas City"):
-    requests_url= f'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={os.getenv("API_KEY")}&q={city}'
+
+    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
 
     weather_data = requests.get(request_url).json()
 
     return weather_data
 
+
 if __name__ == "__main__":
-    print('\n***Get weather Conditions ***\n')
+    print('\n*** Get Current Weather Conditions ***\n')
 
     city = input("\nPlease enter a city name: ")
+
+    # Check for empty strings or string with only spaces
+    # This step is not required here
+    # if not bool(city.strip()):
+    #     city = "Kansas City"
 
     weather_data = get_current_weather(city)
 
